00c5411d1cc8b77f63bd6af732b156f2bc32d4fc
abianfikri <abronjoy46@gmail.com>
2023-06-23 02:19:38 +0700

    api : create, update, delete data

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\CategoryController;
 use App\Http\Controllers\API\ProfileController;
 use App\Http\Controllers\API\TodoController;
 use Illuminate\Http\Request;
@@ -23,6 +24,10 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
     // Route::resource('/todo', TodoController::class);
     Route::apiResource('/todo', TodoController::class);
+    Route::get('/category', [CategoryController::class, 'index'])->name('category.index');
+    Route::patch('/todo/{todo}/complete', [TodoController::class, 'complete'])->name('todo.complete');
+    Route::patch('/todo/{todo}/uncomplete', [TodoController::class, 'uncomplete'])->name('todo.uncomplete');
+    Route::delete('/todo', [TodoController::class, 'deleteAllCompleted'])->name('todo.deleteAllCompleted');
 });
 
 
